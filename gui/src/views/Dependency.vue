b1da84f7128a1573b28e78c1c6367192b6c16d2d
yutod <yutodoi.seattle@gmail.com>
2019-03-11 23:55:00 +0900

    1. Add a search bar for available formula list 2. Add an uninstall confirmation modal

--- gui/src/views/Dependency.vue
+++ gui/src/views/Dependency.vue
@@ -23,17 +23,22 @@
                           <span class="font-weight-bold">{{ item.version.current }}</span>
                         </span>
                       </v-flex>
-                      <v-flex xs3 align-self-center>
+                      <!-- <v-flex xs3 align-self-center>
                         <span class="subheading">latest:
                           <span class="font-weight-bold">{{ item.version.latest }}</span>
                         </span>
-                      </v-flex>
+                      </v-flex> -->
                       <v-flex xs3 align-self-center>
                         <v-icon :size="15" color="green" class="text-md-center">fas fa-check</v-icon>
                         <span class="subheading mx-2">Installed</span>
                       </v-flex>
                       <v-flex align-self-center>
                         <v-btn round outline color="indigo">Upgrade</v-btn>
+                      </v-flex>
+                      <v-flex align-self-center>
+                        <v-btn flat icon @click.stop="dialog = true; uninstall = item.name">
+                          <v-icon>far fa-trash-alt</v-icon>
+                        </v-btn>
                       </v-flex>
                     </v-layout>
                   </v-list-tile-sub-title>
@@ -42,8 +47,34 @@
             </v-list>
             <!-- <v-card-text></v-card-text> -->
           </v-card>
+          <v-dialog v-model="dialog" max-width="520">
+            <v-card>
+              <v-card-title class="headline font-weight-thin" primary-title>Confirm Uninstall</v-card-title>
+              <v-card-text class="title font-weight-light pa-5">Are you sure to uninstall <span class="font-weight-medium font-italic">{{ uninstall }}</span> ?</v-card-text>
+              <v-card-actions class="pa-4">
+                <v-spacer></v-spacer>
+                <v-btn outline color="grey darken-1" flat="flat" @click="dialog = false">Cancel</v-btn>
+                <v-btn color="error darker-1" @click="dialog = false">Uninstall</v-btn>
+              </v-card-actions>
+            </v-card>
+          </v-dialog>
         </v-tab-item>
         <v-tab-item :key="'available'">
+          <v-flex offset-xs1 xs10>
+            <v-card-text>
+              <v-autocomplete
+                v-model="searchKeyword"
+                :hint="!isEditing ? 'Filter by keyword' : 'Click the icon to save'"
+                :items="states"
+                :label="`Search...`"
+                :multiple="true"
+                chips
+                persistent-hint
+                clearable
+              >
+              </v-autocomplete>
+            </v-card-text>
+          </v-flex>
           <v-card>
             <v-list two-line>
               <v-list-tile v-for="formula in formulaPerPage(page)" :key="formula.name" class="my-3">
@@ -126,24 +157,39 @@ export default Vue.extend({
   },
   data() {
     return {
+      isEditing: false,
+      dialog: false,
+      uninstall: '',
+      // searchKeyword: null,
+      searchKeyword: '',
       model: null,
+      states: [] as any[],
       page: 1,
       formulas: [] as Formulas,
     }
   },
   methods: {
     maxPage(): number {
-      return Math.floor(this.formulas.length / countPerPage)
+      return Math.floor(this.filteredFormula.length / countPerPage)
     },
     formulaPerPage(page: number): Formula[] {
-      const startIndex: number = countPerPage * page
+      const startIndex: number = countPerPage * (page - 1)
 
-      return this.formulas.slice(startIndex, startIndex + countPerPage)
+      return this.filteredFormula.slice(startIndex, startIndex + countPerPage)
+    },
+  },
+  computed: {
+    filteredFormula(): Formulas {
+      if (this.searchKeyword && this.searchKeyword.length > 0) {
+        return this.formulas.filter((formula) => this.searchKeyword.includes(formula.name))
+      }
+      return this.formulas
     },
   },
   created() {
     Vue.axios.get(`${formulaEndpoint}`).then((response) => {
       response.data.forEach((element: Response) => {
+        this.states.push(element.name)
         const formula: Formula = {
           name: element.name,
           stable: element.versions.stable,
